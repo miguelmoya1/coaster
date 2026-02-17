e19e14707b32346f9feec8e6ec40514bf4a5ed52
miguelmo <miguelmoyaortega@gmail.com>
2026-02-17 17:37:57 +0100

    chore: Install and update project dependencies.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.barflow.app',
+  appName: 'Coaster',
+  webDir: 'dist'
+};
+
+export default config;
