4b56e42956ed629f5d2841358a91761272b94ab8
miguelmo <miguelmoyaortega@gmail.com>
2026-02-19 16:46:50 +0100

    feat: add `changeStaffPin` callable function.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,16 @@
 import { setGlobalOptions } from 'firebase-functions';
 import { onCall } from 'firebase-functions/https';
-import { createBarHandler } from './handlers/create-bar.handler';
-import { createStaffHandler } from './handlers/create-staff.handler';
 import { initializeApp } from 'firebase-admin';
-import { validateStaffHandler } from './handlers/validate-staff.handler';
 
 initializeApp();
-
 setGlobalOptions({ maxInstances: 10 });
+
+import { createBarHandler } from './handlers/create-bar.handler';
+import { createStaffHandler } from './handlers/create-staff.handler';
+import { validateStaffHandler } from './handlers/validate-staff.handler';
+import { changeStaffPinHandler } from './handlers/change-staff-pin.handler';
 
 export const createBar = onCall(createBarHandler);
 export const createStaff = onCall(createStaffHandler);
 export const validateStaff = onCall(validateStaffHandler);
+export const changeStaffPin = onCall(changeStaffPinHandler);
