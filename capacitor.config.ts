bf84c3aaf9106049167af6ec6ed2b497325c24ab
miguelmo <miguelmoyaortega@gmail.com>
2026-02-24 20:33:18 +0100

    chore: remove all project files.

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.barflow.app',
-  appName: 'Coaster',
-  webDir: 'dist'
-};
-
-export default config;
--- functions/src/tools/generate-staff-uid.tool.ts
+++
@@ -1,5 +0,0 @@
-import { randomBytes } from 'node:crypto';
-
-export const generateStaffUid = () => {
-  return `staff_${randomBytes(4).toString('hex')}`;
-};
